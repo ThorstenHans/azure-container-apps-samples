1909f392bc6f585ccbbc90b4da4b55f7dd6db828
Thorsten Hans <thorsten.hans@gmail.com>
2021-12-15 10:00:26 +0100

    chore: fix destroy.sh for 2nd sample
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- 002-aca-single-container/destroy.sh
+++ 002-aca-single-container/destroy.sh
@@ -3,7 +3,6 @@
 set -e
 
 rgName="rg-aca-single-container"
-location="northeurope"
 
 echo "Creating Resource Group..."
-az group delete -n $rgName -l $location --yes --no-wait
+az group delete -n $rgName --yes --no-wait
